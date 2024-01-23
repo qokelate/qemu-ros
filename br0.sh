ea6f25ced609fe1a06fbf0aaaa3ed4551e5e7949
sma11case <udf.q@qq.com>
2024-01-23 17:26:52 +0800

    v1

--- br0.sh
+++ br0.sh
@@ -16,11 +16,12 @@ macaddr=`nmcli -g GENERAL.HWADDR device show eth0|tr -d '\\\\'`
 gateway=`nmcli -g IP4.GATEWAY device show eth0`
 address=`nmcli -g IP4.ADDRESS device show eth0`
 
+gateway1=`nmcli -g IP4.ADDRESS device show eth0|grep -oE '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'`
 mask0=`nmcli -g IP4.ADDRESS device show eth0|grep -oE '/.+$'`
 address0=`nmcli -g IP4.ADDRESS device show eth0|grep -oE '^[0-9]+\.[0-9]+\.[0-9]+'`
 address1="$address0.2$mask0"
 echo "[INFO] IP $address ==> $address1"
-nmcli connection modify br0 ipv4.addresses "$address1" ipv4.gateway "$gateway" ipv4.dns "$gateway" ipv4.method manual ipv6.method disabled
+nmcli connection modify br0 ipv4.addresses "$address1" ipv4.gateway "$gateway1" ipv4.dns "$gateway1" ipv4.method manual ipv6.method disabled
 
 #生效
 # nmcli device reapply br0
