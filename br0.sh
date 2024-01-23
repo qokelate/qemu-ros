5d24e44b161f7abfea203f12ff0c9f0debfbc8ff
sma11case <udf.q@qq.com>
2024-01-23 17:12:07 +0800

    v1

--- br0.sh
+++ br0.sh
@@ -7,17 +7,17 @@ nmcli connection add type bridge ifname br0 con-name br0
 #关闭stp
 nmcli connection modify br0 bridge.stp no
 
+#设置IP
+nmcli connection modify br0 ipv6.method disabled
+# nmcli connection modify br0 ipv4.method auto
+
 #获取当前数据
 macaddr=`nmcli -g GENERAL.HWADDR device show eth0|tr -d '\\\\'`
 gateway=`nmcli -g IP4.GATEWAY device show eth0`
 address=`nmcli -g IP4.ADDRESS device show eth0`
 
-#设置IP
-nmcli connection modify br0 ipv6.method disabled
-# nmcli connection modify br0 ipv4.method auto
-
 mask0=`nmcli -g IP4.ADDRESS device show eth0|grep -oE '/.+$'`
-address0=`nmcli -g IP4.ADDRESS device show eth0|grep -oE '^[0-9\.]+\.[0-9\.]+\.[0-9\.]+'`
+address0=`nmcli -g IP4.ADDRESS device show eth0|grep -oE '^[0-9]+\.[0-9]+\.[0-9]+'`
 address1="$address0.2$mask0"
 echo "[INFO] IP $address ==> $address1"
 nmcli connection modify br0 ipv4.addresses "$address1" ipv4.gateway "$gateway" ipv4.dns "$gateway" ipv4.method manual ipv6.method disabled
