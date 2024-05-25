96915930199ed965d992cf8e398f76376786a9d8
qokelate <udf.q@qq.com>
2024-05-25 12:56:48 +0800

    Update 0.ubuntu.sh

--- 0.ubuntu.sh
+++ 0.ubuntu.sh
@@ -2,6 +2,20 @@
 
 apt install -y network-manager || exit
 
+rm -rf /etc/netplan
+mkdir -pv /etc/netplan
+cat <EOF>/etc/netplan/99-netcfg.yaml
+network:
+  version: 2
+  renderer: NetworkManager
+  ethernets:
+    alleths:
+      match:
+        name: en*
+      dhcp4: true
+      dhcp6: false
+EOF
+
 systemctl stop systemd-networkd.service
 systemctl disable systemd-networkd.service
 systemctl mask systemd-networkd.service
@@ -9,7 +23,7 @@ systemctl unmask NetworkManager
 systemctl enable NetworkManager
 systemctl restart NetworkManager
 
-netplan apply
+#netplan apply
 
 exit
 
